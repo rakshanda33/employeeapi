9096125c967652a8aabe15effebdf15b5aaf3c60
Rakshanda Noor <rakshandanoor20@gmail.com>
2025-12-16 00:47:25 +0530

    Initial commit: Spring Boot Employee API running

---
+++ src/main/java/com/rakshanda/employeeapi/HelloController.java
@@ -0,0 +1,13 @@
+package com.rakshanda.employeeapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Employee API is running 🚀";
+    }
+}
